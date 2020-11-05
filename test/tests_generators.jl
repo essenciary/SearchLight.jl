5f80013079e4b0a21c24f0cb17691f3019652714
FrankUrbach <frank.urbach@gmx.de>
2020-11-05 13:10:02 +0100

    Add entry testModels to struct setting in Configurations.jl and add test_Models.jl

--- test/tests_generators.jl
+++ test/tests_generators.jl
@@ -8,7 +8,9 @@
     @test isfile(joinpath("app", "resources", "foos", "Foos.jl")) == true
 
     @test isdir(joinpath("db", "migrations")) == true
-    @test occursin("_create_table_foos.jl", Base.Filesystem.readdir(joinpath("db", "migrations"))[1]) == true
+
+    occurenceOfCreate = occursin.("_create_table_foos.jl", Base.Filesystem.readdir(joinpath("db", "migrations")))
+    @test length(filter!(x -> x == 1, occurenceOfCreate)) >= 0
     @test length(SearchLight.Migrations.downed_migrations()) == 1
 
 
